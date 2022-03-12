517a894a8ea7af3de7dabdd57ced471fa32b8405
ambrojessica <ambrojessica0@gmail.com>
2022-03-12 02:28:27 -0600

    first push

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ac from './images/ac.png';
+import { Navbar, Container } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <Navbar>
+      <Container>
+        <Navbar.Brand href="#home">
+          <img
+            alt="animal crossing leaf"
+            src={ac}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />
+          Animal Crossing Villagers
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
