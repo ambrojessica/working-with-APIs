1ef9ebe9829d0e250f36bae4e8b37652e9cc6fda
ambrojessica <ambrojessica0@gmail.com>
2022-03-25 23:47:36 -0500

    reverting changes

--- src/components/ArtWork.js
+++ src/components/ArtWork.js
@@ -1,27 +1,15 @@
 import React from "react";
-import { Row, Col, Card } from "react-bootstrap";
+import ArtList from './ArtList';
 
 const ArtWork = ({ items }) => {
 
   return (
     <main>
-      {items.map((item) => (
-        <Row xs={1} md={2} className="g-4">
-          {Array.from({ length: 1 }).map((_, idx) => (
-            <Col>
-              <Card style={{ width: '18rem' }} >
-                <Card.Img variant="top" src={item.image_uri} />
-                <Card.Body>
-                  <Card.Title>{item.name["name-USen"]}</Card.Title>
-                  <Card.Text>
-                    {item["museum-desc"]}
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      ))}
+      {
+        items.map((item) => (
+          <ArtList item={item} key={item.id} />
+        ))
+      }
     </main>
 
   );
